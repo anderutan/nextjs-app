db2556b854fd93ad057e75b27a0c72f3e41c1b26
anderu <andrewtan1310@gmail.com>
2024-05-23 11:58:52 +0800

    Add homepage and footer

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from 'next/image';
+import styles from './about.module.css';
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src='https://images.pexels.com/photos/21655968/pexels-photo-21655968/free-photo-of-people-walking-in-rain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+          alt=''
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
